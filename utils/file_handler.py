53700176e98609bba362596a40a5719483ff6629
Zoya Ahmed <zoyaahm.work@gmail.com>
2026-01-20 21:40:47 +0530

    Update file_handler.py

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -1,1 +1,14 @@
+def read_sales_file(file_path):
+    records = []
+
+    try:
+        with open(file_path, "r", encoding="latin-1") as file:
+            for line in file:
+                line = line.strip()
+                if line:
+                    records.append(line)
+    except Exception as e:
+        print("Error reading file:", e)
+
+    return records
 
